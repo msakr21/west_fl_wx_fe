3ebd8ed70d74c2b0f420e35f41a1bdf880b128af
Alex M <alexm0717@hotmail.com>
2023-01-11 09:31:01 -0800

    Update test

--- spec/features/pages/welcome_spec.rb
+++ spec/features/pages/welcome_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Root page' do
   it 'has name of the app ' do
     visit '/'
-    expect(page).to have_content('West FL WX')
+    expect(page).to have_content('West Florida')
   end
 
   it 'has a link to register new users ' do
     visit '/'
-    expect(page).to have_link('Register')
+    expect(page).to have_link('Login with Google')
   end
 
   it 'has a link for registered users to log in  ' do
